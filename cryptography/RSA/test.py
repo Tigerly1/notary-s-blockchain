902169263c7698530acff6b075d1ba379134da2b
bartekf4 <56221703+bartekf4@users.noreply.github.com>
2022-12-04 14:28:30 +0100

    add cryptography package

---
+++ cryptography/RSA/test.py
@@ -0,0 +1,24 @@
+from cryptography.RSA import *
+
+
+def from_hex(cls, h):
+    return cls(hex_to_bytes(h))
+
+
+private, public = generate_keypair(512)
+
+txt = 'deadbeef'
+message = Message.from_hex(txt)
+message.encrypt(public)
+print("Encrypted message", message)
+message.decrypt(private)
+print("Decrypted message", message.hex())
+
+
+message.encrypt(private)
+message.decrypt(public)
+
+message = Message.from_str('Very important message')
+signature = message.sign(private)
+print(message.verify(signature, public))
+assert message.verify(signature, public)
